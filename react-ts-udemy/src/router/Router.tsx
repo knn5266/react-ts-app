5ecab50dc8c0626bb084217cccbbcc9fb39b248f
Kanon <mkc35r.0@gmail.com>
2024-05-01 16:34:39 +0900

    タイポ修正

--- react-ts-udemy/src/router/Router.tsx
+++ react-ts-udemy/src/router/Router.tsx
@@ -29,7 +29,7 @@ export const Router:VFC = memo(() => {
       )} >
         </Route>
         </LoginUserProvider>
-        </UserGua>
+        </UserGuard>
         <Route path='*' >
           <Page404 />
         </Route>
